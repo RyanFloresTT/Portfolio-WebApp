e1c717ebaa153532abb8c1c411df081ee89efcae
Ryan Flores <rryanflorres@gmail.com>
2024-01-14 15:11:40 -0800

    Add Category View

---
+++ PortfolioWeb/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioWeb.Data;
+using PortfolioWeb.Models;
+
+namespace PortfolioWeb.Controllers {
+    public class CategoryController : Controller {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index() {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
