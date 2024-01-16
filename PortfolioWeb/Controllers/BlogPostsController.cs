a7bcb223176dc9372cd4dba0167072d13dff4ecc
Ryan Flores <rryanflorres@gmail.com>
2024-01-15 23:23:21 -0800

    Closes #3, Closes #4, Closes #5, Closes #6, Closes #7

---
+++ PortfolioWeb/Controllers/BlogPostsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PortfolioWeb.Data;
+using PortfolioWeb.Models;
+
+namespace PortfolioWeb.Controllers
+{
+    public class BlogPostsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogPostsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BlogPosts
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.BlogPost.Include(b => b.Project);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: BlogPosts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPost
+                .Include(b => b.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjectId"] = new SelectList(_context.Set<Project>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: BlogPosts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Body,Summary,CreatedAt,ProjectId,Tags")] BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(blogPost);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Set<Project>(), "Id", "Id", blogPost.ProjectId);
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPost.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Set<Project>(), "Id", "Id", blogPost.ProjectId);
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Body,Summary,CreatedAt,ProjectId,Tags")] BlogPost blogPost)
+        {
+            if (id != blogPost.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blogPost);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogPostExists(blogPost.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Set<Project>(), "Id", "Id", blogPost.ProjectId);
+            return View(blogPost);
+        }
+
+        // GET: BlogPosts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var blogPost = await _context.BlogPost
+                .Include(b => b.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogPost);
+        }
+
+        // POST: BlogPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var blogPost = await _context.BlogPost.FindAsync(id);
+            if (blogPost != null)
+            {
+                _context.BlogPost.Remove(blogPost);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogPostExists(int id)
+        {
+            return _context.BlogPost.Any(e => e.Id == id);
+        }
+    }
+}
