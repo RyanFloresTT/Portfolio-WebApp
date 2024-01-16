5914ac59ace6d4f131b40018827d3c79e43b38f8
Ryan Flores <rryanflorres@gmail.com>
2024-01-16 00:17:56 -0800

    Initial Design for Project & Model

--- PortfolioWeb/obj/Debug/net8.0/PortfolioWeb.AssemblyInfo.cs
+++ PortfolioWeb/obj/Debug/net8.0/PortfolioWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d3ac53fb8b4c2143a19f69f34ba6b3a0236f3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7bcb223176dc9372cd4dba0167072d13dff4ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
